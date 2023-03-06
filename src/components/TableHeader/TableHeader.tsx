db691f0033df4865edcc3f5300ed475c8bebaf3d
joaoppsantos <joaoppsantos@live.com>
2023-03-06 12:39:45 +0000

    feat(keywords): Add table column heading and respective types

--- src/components/TableHeader/TableHeader.tsx
+++ src/components/TableHeader/TableHeader.tsx
@@ -1,11 +1,24 @@
 import { Box, Flex } from '@chakra-ui/react';
+import { HeaderColumnType, TableColumns } from '../../utils/types';
+import { ColumnSort } from '../ColumnSort/ColumnSort';
 import { TableElement } from '../TableElement/TableElement';
 
 export const TableHeader = () => {
   return (
     <Box>
-      <Flex flexDirection="row">
-        <TableElement>Teste</TableElement>
+      <Flex>
+        <TableElement>
+          {<ColumnSort text={TableColumns.keyword} />}
+        </TableElement>
+        <TableElement>
+          {<ColumnSort text={TableColumns.competition} />}
+        </TableElement>
+        <TableElement>
+          {<ColumnSort text={TableColumns.search_volume} />}
+        </TableElement>
+        <TableElement>
+          {<ColumnSort text={TableColumns.overall_score} />}
+        </TableElement>
       </Flex>
     </Box>
   );
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,3 +3,14 @@ import { ReactNode } from 'react';
 export interface TableElementProps {
   children: string | JSX.Element;
 }
+
+export type HeaderColumnType = {
+  [key: string]: ReactNode;
+};
+
+export enum TableColumns {
+  keyword = 'Keywords',
+  competition = 'Competition',
+  overall_score = 'Overall Score',
+  search_volume = 'Search volume',
+}
